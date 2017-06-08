1fa9023d62d8dfca00f5276eff13868ddc057811
Igor Omelchenko <counter3d@gmail.com>
2017-06-08 19:55:29 +0300

    Version 0.1.2

--- travian4api/__init__.py
+++ travian4api/__init__.py
@@ -7,7 +7,7 @@ from .servers import servers
 
 logger = logging.getLogger(__name__)
 
-__tuple_version__ = (0, 1, 1)
+__tuple_version__ = (0, 1, 2)
 __version__ = '.'.join((str(i) for i in __tuple_version__))
 
 logger.debug('Travian Legends API version: ' + __version__)
